7a6ff511ed6cdc7a98edbb8c9e45c50c238e957e
evanbest0802 <evanbest0802@gmail.com>
2019-02-13 10:43:08 +0800

    Use UDP to send packet.

--- tcpcli08.c
+++ tcpcli08.c
@@ -5,20 +5,26 @@ main(int argc, char **argv)
 {
 	int					sockfd;
 	struct sockaddr_in	servaddr;
-
+	char	sendline[1500];
 	if (argc != 2)
 		err_quit("usage: tcpcli <IPaddress>");
 
-	sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(5202);
-	Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
-	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-	str_cli(stdin, sockfd);		/* do it all */
+	//str_cli(stdin, sockfd);		/* do it all */
 
+
+	while (fgets(sendline, 1500, stdin) != NULL) {
+
+		write(sockfd, sendline, strlen(sendline));
+		printf("packet send\n");
+	}
 	exit(0);
 }
--- tcpserv01.c
+++ tcpserv01.c
@@ -1,78 +1,96 @@
-#include	"unp.h"
+//#include	"unp.h"
+#include	<sys/types.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>	
+#include	<arpa/inet.h>	
+#include	<errno.h>
+#include	<errno.h>
+#include	<fcntl.h>		
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	
+#include	<sys/uio.h>		
+#include	<unistd.h>
+#include	<sys/wait.h>
+#include	<sys/un.h>
 #include	<ifaddrs.h>
 int
 main(int argc, char **argv)
 {
-	int					listenfd, connfd,loopbackfd,n;
+	int					listenfd, connfd,udpfd,n;
 	pid_t				childpid;
 	socklen_t			clilen;
-	struct sockaddr_in	cliaddr, servaddr,loopbackaddr,localaddr;
-	char		line[MAXLINE],peeraddress[INET_ADDRSTRLEN];
-	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in	cliaddr, servaddr,remoteaddr,localaddr;
+	char		line[1500],peeraddress[INET_ADDRSTRLEN];
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(5202);
 
-	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+	bind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
 
-	Listen(listenfd, LISTENQ);
+	//listen(listenfd, 100);
 
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
+		connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen);
 		inet_ntop(AF_INET, &cliaddr.sin_addr,peeraddress,INET_ADDRSTRLEN);
 		if(!strcmp("127.0.0.1",peeraddress))
 		{
 			printf("loopback address\n");
-			Close(connfd);
+			close(connfd);
 			continue;
 		}
-		if ( (childpid = Fork()) == 0) 
+		if ( (childpid = fork()) == 0) 
 		{	/* child process */
-			Close(listenfd);	/* close listening socket */
+			close(listenfd);	/* close listening socket */
 			//str_echo(connfd);	/* process the request */
-			printf("in for\n");
 			again:
-			while((n=Read(connfd,line,MAXLINE))>0)
+			while((n=read(connfd,line,1500))>0)
 			{
-				struct ifaddrs * ifaddrstruct = NULL;
+				/*struct ifaddrs * ifaddrstruct = NULL;
 				void * tmpaddrptr = NULL;
 				char addressBuffer[INET_ADDRSTRLEN];
 				getifaddrs(&ifaddrstruct);
 				while(ifaddrstruct != NULL)
-				{	
-					if(ifaddrstruct->ifa_addr->sa_family==AF_INET)
+				{
+					if((ifaddrstruct->ifa_addr->sa_family)==AF_INET)
 					{
-						tmpaddrptr=&((struct sockaddr_in *)ifaddrstruct->ifa_addr)->sin_addr;
-						
+						printf("dsafasdfasfasdfasdfaf\n");
+						tmpaddrptr=&((struct sockaddr_in *)ifaddrstruct->ifa_addr)->sin_addr;						
 						inet_ntop(AF_INET, tmpaddrptr, addressBuffer, INET_ADDRSTRLEN);
-						if(!strcmp(ifaddrstruct->ifa_name,"enp0s9"))break;
-					}
+						if(!strcmp(ifaddrstruct->ifa_name,"enp0s9")){printf("got it\n");break;}
+							
+					}printf("find next\n");
 					ifaddrstruct=ifaddrstruct->ifa_next;
-				}
+					printf("point to next\n");
+				}*/
 				printf("after read\n");
-				loopbackfd = Socket(AF_INET, SOCK_STREAM, 0);
-				bzero(&loopbackaddr, sizeof(loopbackaddr));
-				loopbackaddr.sin_family = AF_INET;
-				loopbackaddr.sin_port = htons(5202);
-				Inet_pton(AF_INET, "8.8.8.8", &loopbackaddr.sin_addr);
-				Inet_pton(AF_INET, addressBuffer, &localaddr.sin_addr);
-				bind(loopbackfd,(SA *) &localaddr, sizeof(localaddr));
-				//printf("%d\n",setsockopt(loopbackfd,SOL_SOCKET,SO_BINDTODEVICE,"lo",2));
-				printf("before connect\n");
-				Connect(loopbackfd, (SA *) &loopbackaddr, sizeof(loopbackaddr));
-				printf("after\n");
-				Write(loopbackfd, line, strlen(line));
+				udpfd = socket(AF_INET, SOCK_DGRAM, 0);
+				bzero(&remoteaddr, sizeof(remoteaddr));
+				remoteaddr.sin_family = AF_INET;
+				remoteaddr.sin_port = htons(5202);
+				inet_pton(AF_INET, "127.0.0.15", &remoteaddr.sin_addr);
+				//Inet_pton(AF_INET, addressBuffer, &localaddr.sin_addr);
+				//bind(udpfd,(SA *) &localaddr, sizeof(localaddr));
+				printf("%d\n",setsockopt(udpfd,SOL_SOCKET,SO_BINDTODEVICE,"enp0s9",6));
+				//printf("before connect\n");
+				//Connect(loopbackfd, (SA *) &remoteaddr, sizeof(remoteaddr));
+				//printf("after\n");
+				//Write(udpfd, line, strlen(line));
+				sendto(udpfd,line,strlen(line),0,(struct sockaddr*)&remoteaddr,sizeof(remoteaddr));
 				bzero(&line, strlen(line));
-				Close(loopbackfd);
+				close(udpfd);
 			}
 			if(n<0 && errno==EINTR)goto again;
 			else if (n<0) printf("errorr\n");
-			printf("child exit\n");
 			exit(0);
 		}
-		Close(connfd);			/* parent closes connected socket */
+		close(connfd);			/* parent closes connected socket */
 	}
 }
